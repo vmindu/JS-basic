c8c96b5faa8b46681207e82da670e4305d37e79b
Vrutsal Mistry <103307784+vmindu@users.noreply.github.com>
2024-06-06 09:46:18 +0000

    conversion

---
+++ 01_basic/03_conversionOpration.js
@@ -0,0 +1,37 @@
+let score = 33
+
+// console.log(typeof score)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1, false = 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true,0 => false
+// "" => false
+// "dsjvbhjdb" => true
+
+// *******************************operations*******************************************
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "efvewv"
+let str2 = " evekvn"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+let efvwe = 100
+++efvwe
+console.log(efvwe);
