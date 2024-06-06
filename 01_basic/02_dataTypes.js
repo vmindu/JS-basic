ad854ee9ca613cf6a1de2ffeb66138ead258bfe4
Vrutsal Mistry <103307784+vmindu@users.noreply.github.com>
2024-06-06 07:05:04 +0000

    dataTypes

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -6,3 +6,11 @@ console.log(3 + 3)
 // code readability are should be high
 
 console.log("vrutsal")
+
+// String => ""
+// Number => 13 // 2 to power 53
+// boolean => true, false
+// bigint => big number 
+// null => stand alon value typeof Object
+// undefined => value are not Put
+// Symboel => unique 
