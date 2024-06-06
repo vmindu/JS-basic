37e4a1e1d3496658fb20d6518e3cdbf45fe73d87
Vrutsal Mistry <103307784+vmindu@users.noreply.github.com>
2024-06-06 07:01:46 +0000

    dataTypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 14553
+let accountEmail = "vrutsall@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+// accountId = 2 not allowed
+
+accountEmail = "whsbfuw@gmail.com"
+accountPassword = "3224"
+accountCity = "ahmedabad"
+
+/*prefer not using var in javascript because of issue in block scope and functional scope */
+
+console.table([accountId, accountPassword, accountEmail, accountCity])
+
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,8 @@
+"use strict"; // treat all  js code as newer version
+
+// alert(3 + 3) we are using node js not browers    
+
+console.log(3 + 3)
+// code readability are should be high
+
+console.log("vrutsal")
